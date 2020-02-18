3f428ebef08f399e10cfa951c173708ef3dd071b
Taahir1738 <hiedie786@gmail.com>
2020-02-18 09:52:56 +0200

    second commit

---
+++ test.sql
@@ -0,0 +1,11 @@
+Chips,Quantity,Price,Cooldrink,Quantity,Price,Chocolate,Quantity,Price,Pies,Quantity	,Price	,Fruit	,Quantity,Price,Cupcakes,Quantity,Price,Veggies,Quantity,Price
+Simba	2,R4,50,Coke,20,R10,00,Cadbury,18,R15,00,Pepper Steak,20,R20,00	Pear,5,R3,00,Vanilla,15	R4,00,Spinach,8,R15,00
+Lays	4	R6,00	Fanta	15	R10,00	Snickers	20	R6,00	Chicken	13	R15,00	Apple	20	R3,00	Chocolate	20	R4,00	Cabbage	7	R15,00
+Willards	6	R5,00	Jive	10	R8,00	Lindt	15	R20,00	Steak	15	R15,00	Orange	12	R3,00	Lemon	10	R3,00	Carrots	15	R13,00
+Pringles	8	R10,00	Tab	8	R13,00	Ferrero Rocher	10	R25,00	Chicken & Mushroom	5	R20,00	Banana	15	R5,00	Caramel	13	R4,00	Broccoli	14	R15,00
+Doritos	10	R6,00	Sprite	13	R10,00	Nestle	15	R10,00	Curry	6	R15,00	Plum	10	R3,00	Toffee	6	R3,00	Garlic	10	R10,00
+Messaris	12	R3,50	Sparletta	6	R8,00	Toblerone	9	R18,00	Sausage roll	10	R15,00	Lychee	10	R0,50	Red Velvet	20	R5,00	Brussel Sprouts		R10,00
+Nik Naks	14	R2,00	Stoney	5	R10,00	Godiva	8	R15,00	Burger 	5	R13,00	Papaya	5	R3,00	Strawberry	15	R4,00	Peas	10	R10,00
+IQ Foods	9	R1,50	Schweppes	3	R9,00	Kit kat	18	R10,00	Cornish	7	R13,00	Grape	15	R5,00	Coconut	14	R3,00	Asparagus	10	R10,00
+Walkers	10	R2,00	Twizza	5	R10,00	De Villiers	9	R20,00	Beef Rib	6	R15,00	Mango	18	R7,00	Pineapple	13	R3,00	Potatoes	28	R5,00
+Truda	12	R2,00	Cape Cola	6	R8,00	Merci	8	R20,00	Salami & Cheese	5	R13,00	Pineapple	7	R10,00	Peppermint	15	R3,00	Peppers	20	R3,00
